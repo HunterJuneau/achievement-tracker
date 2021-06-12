ce4e4a210421d19bdcd7cb0abc677d610c409e45
HunterJuneau <huntlee17@gmail.com>
2021-06-12 10:39:01 -0500

    Debugging

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase';
 import { BrowserRouter } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Routes from '../helpers/Routes';
@@ -6,10 +7,27 @@ import Routes from '../helpers/Routes';
 import './App.scss';
 
 function App() {
+  const [user, setUser] = useState(false);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        console.warn(authed);
+        const userInfoObj = {
+          fullName: authed.displayName,
+          uid: authed.uid,
+        };
+        setUser(userInfoObj);
+      } else if (user === null) {
+        setUser(false);
+      }
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <NavBar />
-      <Routes />
+      <Routes setUser={setUser} />
     </BrowserRouter>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
-  Navbar, NavbarBrand, Nav, NavItem, NavLink
+  Navbar, NavbarBrand, Nav, NavItem, NavLink, Button
 } from 'reactstrap';
+import { signOutUser } from '../helpers/auth';
 
 export default function NavBar() {
   return (
@@ -15,6 +16,9 @@ export default function NavBar() {
           <NavLink href='/games'>Games</NavLink>
         </NavItem>
       </Nav>
+      <Button onClick={signOutUser} color='warning'>
+        Log Out
+      </Button>
     </Navbar>
   );
 }
