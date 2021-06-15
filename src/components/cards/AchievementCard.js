e76481ddb40f035d38c2269db42d0b49c1aa7e6f
HunterJuneau <huntlee17@gmail.com>
2021-06-15 17:51:59 -0500

    Added achievements to Achievements View.

---
+++ src/components/cards/AchievementCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function AchievementCard({
+  achieved, img, name, description
+}) {
+  return (
+    <tr>
+      <th scope='row'>
+        <img height='30px' src={img} alt={name} />
+      </th>
+      <th>{name}</th>
+      <th>{description}</th>
+      <th>{achieved ? 'Yes' : 'No'}</th>
+    </tr>
+  );
+}
+
+AchievementCard.propTypes = {
+  img: PropTypes.string,
+  name: PropTypes.string,
+  description: PropTypes.string,
+  achieved: PropTypes.bool,
+};
--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -3,18 +3,35 @@ import { Switch, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import Games from '../views/Games';
 import Unauthenticated from '../views/Unauthenticated';
+import Achievements from '../views/Achievements';
 
 export default function Routes({ user }) {
+  const [achievements, setAchievements] = useState([]);
   const [games, setGames] = useState([]);
 
   return (
     <>
       <Switch>
         {user ? (
-          <Route
-            path='/games'
-            component={() => <Games uid={user.uid} games={games} setGames={setGames} />}
-          />
+          <>
+            <Route
+              exact
+              path='/'
+              component={() => (
+                <Achievements
+                  uid={user.uid}
+                  achievements={achievements}
+                  setAchievements={setAchievements}
+                />
+              )}
+            />
+            <Route
+              path='/games'
+              component={() => (
+                <Games uid={user.uid} games={games} setGames={setGames} />
+              )}
+            />
+          </>
         ) : (
           <Route path='/' component={Unauthenticated} />
         )}
