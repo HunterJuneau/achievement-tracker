fb76185b2a9fd58ac9e1e961d82eb523c97e2bbd
HunterJuneau <huntlee17@gmail.com>
2021-06-26 03:54:48 -0500

    Fixed Steam URL's.

--- src/helpers/data/steamData.js
+++ src/helpers/data/steamData.js
@@ -6,7 +6,7 @@ const steamKey = firebaseConfig.steamApiKey;
 const getOwnedGames = (steamId) => new Promise((resolve, reject) => {
   axios
     .get(
-      `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${steamKey}&steamid=${steamId}&format=json&include_appinfo=1`,
+      `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${steamKey}&steamid=${steamId}&format=json&include_appinfo=1`,
     )
     .then((response) => {
       resolve(response.data.response.games);
@@ -17,7 +17,7 @@ const getOwnedGames = (steamId) => new Promise((resolve, reject) => {
 const getPlayerAchievements = (appId, steamId) => new Promise((resolve, reject) => {
   axios
     .get(
-      `http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${appId}&key=${steamKey}&steamid=${steamId}&l=english`,
+      `https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${appId}&key=${steamKey}&steamid=${steamId}&l=english`,
     )
     .then((response) => {
       resolve(response.data.playerstats.achievements);
