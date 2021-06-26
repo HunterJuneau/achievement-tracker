71a4c263fdb46e7fefb1d3433993c8717f17ff14
HunterJuneau <huntlee17@gmail.com>
2021-06-26 07:00:16 -0500

    undo

--- src/helpers/data/steamData.js
+++ src/helpers/data/steamData.js
@@ -6,7 +6,7 @@ const steamKey = firebaseConfig.steamApiKey;
 const getOwnedGames = (steamId) => new Promise((resolve, reject) => {
   axios
     .get(
-      `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${steamKey}&steamid=${steamId}&format=json&include_appinfo=1.json`,
+      `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${steamKey}&steamid=${steamId}&format=json&include_appinfo=1`,
     )
     .then((response) => {
       resolve(response.data.response.games);
@@ -17,7 +17,7 @@ const getOwnedGames = (steamId) => new Promise((resolve, reject) => {
 const getPlayerAchievements = (appId, steamId) => new Promise((resolve, reject) => {
   axios
     .get(
-      `https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${appId}&key=${steamKey}&steamid=${steamId}&l=english.json`,
+      `https://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=${appId}&key=${steamKey}&steamid=${steamId}&l=english`,
     )
     .then((response) => {
       resolve(response.data.playerstats.achievements);
